5d69d160edd08f2d278e4f1e50c533e85bce9f6c
Denys-Haliuk <evilian1337@gmail.com>
2022-07-17 02:02:16 +0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.haliuk.genshinexpcalc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.haliuk.genshinexpcalc to javafx.fxml;
+    exports com.haliuk.genshinexpcalc;
+}
